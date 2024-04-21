97341e6ebc4ec8eecd231cdd5096277c7e25b91e
SinzMise <email@sinzmise.top>
2024-04-21 18:39:51 +0800

    Site updated: 2024-04-21 18:39:47

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@ workbox.core.clientsClaim();
 
 // 注册成功后要立即缓存的资源列表
 // 具体缓存列表在gulpfile.js中配置，见下文
-workbox.precaching.precacheAndRoute([{"revision":"fca8ca3a71cae8118309c33a9577e100","url":"./404.html"},{"revision":"f78400332db38e109cf09e3208489819","url":"./index.html"},{"revision":"793e6861668298d001d2eca8ab9a5c97","url":"./js/main.js"},{"revision":"a41547e1d303805be41c4087c26233a9","url":"./css/index.css"}],{
+workbox.precaching.precacheAndRoute([{"revision":"e539e4a0c38a42635c51e5518c292e0d","url":"./404.html"},{"revision":"75e8752ba5d9e8e85bb8e04639679358","url":"./index.html"},{"revision":"793e6861668298d001d2eca8ab9a5c97","url":"./js/main.js"},{"revision":"a41547e1d303805be41c4087c26233a9","url":"./css/index.css"}],{
     directoryIndex: null
 });
 
