367a6c3dfd351118d90025b8990e2b02f0970ca3
SinzMise <email@sinzmise.top>
2024-05-04 16:31:31 +0800

    上传博客

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,12 @@
-var gulp = require('gulp');
-var cleanCSS = require('gulp-clean-css');
-var htmlmin = require('gulp-htmlmin');
-var htmlclean = require('gulp-htmlclean');
-var imagemin = require('gulp-imagemin');
-var workbox = require("workbox-build");
+let gulp = require('gulp')
+let cleanCSS = require('gulp-clean-css')
+let htmlmin = require('gulp-htmlmin')
+let htmlclean = require('gulp-htmlclean')
+let babel = require('gulp-babel') /* 转换为es2015 */
+let uglify = require('gulp-uglify')
+let imagemin = require('gulp-imagemin')
+const workbox = require("workbox-build");
 
-// 若使用babel压缩js，则取消下方注释，并注释terser的代码
-// var uglify = require('gulp-uglify');
-// var babel = require('gulp-babel');
-
-// 若使用terser压缩js
-var terser = require('gulp-terser');
-
-//pwa
 gulp.task('generate-service-worker', () => {
     return workbox.injectManifest({
         swSrc: './sw-template.js',
@@ -29,56 +23,73 @@ gulp.task('generate-service-worker', () => {
         }
     });
 });
+// 设置根目录
+const root = './public'
 
-//minify js babel
-// 若使用babel压缩js，则取消下方注释，并注释terser的代码
-// gulp.task('compress', () =>
-//   gulp.src(['./public/**/*.js', '!./public/**/*.min.js'])
-// 		.pipe(babel({
-// 			presets: ['@babel/preset-env']
-// 		}))
-//     .pipe(uglify().on('error', function(e){
-//       console.log(e);
-//     }))
-// 		.pipe(gulp.dest('./public'))
-// );
+// 匹配模式， **/*代表匹配所有目录下的所有文件
+const pattern = '**/*'
 
-// minify js - gulp-tester
-// 若使用terser压缩js
-gulp.task('compress', () =>
-    gulp.src(['./public/**/*.js', '!./public/**/*.min.js'])
-        .pipe(terser())
-        .pipe(gulp.dest('./public'))
-)
-//css
-gulp.task('minify-css', () => {
-    return gulp.src('./public/**/*.css')
-        .pipe(cleanCSS({
-            compatibility: 'ie11'
-        }))
-        .pipe(gulp.dest('./public'));
-});
-
-
-// 壓縮 public 目錄內 html
-gulp.task('minify-html', () => {
-    return gulp.src('./public/**/*.html')
+// 压缩html
+gulp.task('minify-html', function() {
+    return gulp
+        // 匹配所有 .html结尾的文件
+        .src(`${root}/${pattern}.html`)
         .pipe(htmlclean())
-        .pipe(htmlmin({
-            removeComments: true, //清除 HTML 註釋
-            collapseWhitespace: true, //壓縮 HTML
-            collapseBooleanAttributes: true, //省略布爾屬性的值 <input checked="true"/> ==> <input />
-            removeEmptyAttributes: true, //刪除所有空格作屬性值 <input id="" /> ==> <input />
-            removeScriptTypeAttributes: true, //刪除 <script> 的 type="text/javascript"
-            removeStyleLinkTypeAttributes: true, //刪除 <style> 和 <link> 的 type="text/css"
-            minifyJS: true, //壓縮頁面 JS
-            minifyCSS: true, //壓縮頁面 CSS
-            minifyURLs: true
-        }))
+        .pipe(
+            htmlmin({
+                removeComments: true,
+                minifyJS: true,
+                minifyCSS: true,
+                minifyURLs: true
+            })
+        )
         .pipe(gulp.dest('./public'))
-});
+})
 
-// 執行 gulp 命令時執行的任務
-gulp.task("default", gulp.series("generate-service-worker", gulp.parallel(
-    'compress','minify-html', 'minify-css'
-)));
+// 压缩css
+gulp.task('minify-css', function() {
+    return gulp
+        // 匹配所有 .css结尾的文件
+        .src(`${root}/${pattern}.css`)
+        .pipe(
+            cleanCSS({
+                compatibility: 'ie8'
+            })
+        )
+        .pipe(gulp.dest('./public'))
+})
+
+// 压缩js
+gulp.task('minify-js', function() {
+    return gulp
+        // 匹配所有 .js结尾的文件
+        .src(`${root}/${pattern}.js`)
+        .pipe(
+            babel({
+                presets: ['env']
+            })
+        )
+        .pipe(uglify())
+        .pipe(gulp.dest('./public'))
+})
+
+// 压缩图片
+gulp.task('minify-images', function() {
+    return gulp
+        // 匹配public/images目录下的所有文件
+        .src(`${root}/images/${pattern}`)
+        .pipe(
+            imagemin(
+                [
+                    imagemin.gifsicle({ optimizationLevel: 3 }),
+                    imagemin.jpegtran({ progressive: true }),
+                    imagemin.optipng({ optimizationLevel: 7 }),
+                    imagemin.svgo()
+                ],
+                { verbose: true }
+            )
+        )
+        .pipe(gulp.dest('./public/images'))
+})
+
+gulp.task('default', gulp.series('generate-service-worker', 'minify-html', 'minify-css', 'minify-js'))
