5645aae585b0ee54d546b2cecdadef5a9229739d
SinzMise <email@sinzmise.top>
2024-05-26 16:36:35 +0800

    hexo 全站 NPM 化

---
+++ prescripts/pkgpublish.mjs
@@ -0,0 +1,11 @@
+import { writeFile } from 'fs';
+const pkgfile = {
+    "name": "sinzmise",
+    "version": "1.6.0-"+new Date().getTime()
+}
+writeFile('./public/package.json', JSON.stringify(pkgfile), function (err) {
+    if (err) {
+        console.log(err);
+    }
+    console.log("Package.json file is created successfully.");
+})
