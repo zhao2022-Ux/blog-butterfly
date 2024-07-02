0faeae2cc94504c8fcd7d3a060efc0ceeba5f426
SinzMise <email@sinzmise.top>
2024-07-02 15:07:45 +0800

    优化某些东西

---
+++ deploy-en.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp "config-en.yml" _config.yml
+cp "config-butterfly-en.yml" "_config.butterfly.yml"
+echo "Set en success!"
---
+++ deploy-zh.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp "config-zh.yml" _config.yml
+cp "config-butterfly-zh.yml" "_config.butterfly.yml"
+echo "Set zh success!"
