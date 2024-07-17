b26c741bf0ca8da33b990a2822896b10a75bc403
SinzMise <email@sinzmise.top>
2024-07-17 10:25:01 +0800

    修复live2d的问题

--- source/js/autoload.js
+++ source/js/autoload.js
@@ -1,7 +1,5 @@
 // live2d_path 参数建议使用绝对路径
 const live2d_path = "https://code.webcache.cn/gh/stevenjoezhang/live2d-widget@latest/";
-const live2d_path = "https://code.webcache.cn/gh/stevenjoezhang/live2d-widget@latest/";
-const live2d_path = "https://code.webcache.cn/gh/stevenjoezhang/live2d-widget@latest/";
 //const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
