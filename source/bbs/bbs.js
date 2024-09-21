eb5f836e54bec996a6df20a2357bab5a7cec2462
SinzMise <email@sinzmise.top>
2024-09-21 12:54:44 +0800

    换回来吧。。。。。

--- source/bbs/bbs.js
+++ source/bbs/bbs.js
@@ -5,38 +5,8 @@ function whenDOMReady() {
 whenDOMReady() // 打开网站先执行一次
 document.addEventListener("pjax:complete", whenDOMReady) // pjax加载完成（切换页面）后再执行一次
 
-// 封装异步加载资源的方法
-function loadExternalResource(url, type) {
-    return new Promise((resolve, reject) => {
-        let tag;
-
-        if (type === "css") {
-            tag = document.createElement("link");
-            tag.rel = "stylesheet";
-            tag.href = url;
-        }
-        else if (type === "js") {
-            tag = document.createElement("script");
-            tag.src = url;
-        }
-        if (tag) {
-            tag.onload = () => resolve(url);
-            tag.onerror = () => reject(url);
-            document.getElementById('memobbs-static').appendChild(tag);
-        }
+function loadbbs(){
+    $.get("/bbs/bbs.html",function(data){
+        $("#memosbbs").html(data);
     });
-}
-
-function loadbbs() {
-    // 哔哔广场依赖的css
-    loadExternalResource("https://jsd.cdn.storisinz.site/npm/artalk/dist/ArtalkLite.css", "css"),
-    loadExternalResource("https://memobbs.app/memos.css", "css"),
-    loadExternalResource("https://memobbs.app/grid.css", "css"),
-
-    // 哔哔广场依赖的js
-    loadExternalResource("https://jsd.cdn.storisinz.site/npm/twikoo", "js"),
-    loadExternalResource("https://jsd.cdn.storisinz.site/npm/artalk/dist/ArtalkLite.js", "js"),
-    loadExternalResource("https://jsd.cdn.storisinz.site/npm/marked/marked.min.js", "js"),
-    loadExternalResource("https://jsd.cdn.storisinz.site/npm/lozad/dist/lozad.min.js", "js"),
-    loadExternalResource("https://memobbs.app/memos.js", "js")
 }
--- source/bbs/index.md
+++ source/bbs/index.md
@@ -4,5 +4,5 @@ date: 2024-01-21 21:51:29
 comments: false
 aside: false
 top_img: false
-type: bbs
 ---
+<div id="memosbbs"></div>
