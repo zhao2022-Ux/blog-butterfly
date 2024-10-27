c1f89cabe618ccbcf950a0009620a6324a8bce2b
SinzMise <email@sinzmise.top>
2024-10-27 18:23:34 +0800

    修复压缩问题

--- gulpfile-zh.js
+++ gulpfile-zh.js
@@ -1,46 +1,45 @@
-var gulp = require('gulp');
-var cleanCSS = require('gulp-clean-css');
-var htmlmin = require('gulp-html-minifier-terser');
-var htmlclean = require('gulp-htmlclean');
+const gulp = require('gulp');
+const cleanCSS = require('gulp-clean-css');
+const htmlmin = require('gulp-html-minifier-terser');
+const htmlclean = require('gulp-htmlclean');
+const terser = require('gulp-terser');
+const sourcemaps = require('gulp-sourcemaps');
 
-// 使用terser压缩js
-var terser = require('gulp-terser');
-
-// minify js - gulp-tester
-gulp.task('compress', () =>
-    gulp.src(['./public/**/*.js', '!./public/**/*.min.js'])
-        .pipe(terser())
+// 压缩css文件
+const minify_css = () => (
+    gulp.src(['./public/**/*.css','!./public/{lib,lib/**}','!./public/{libs,libs/**}','!./public/{media,media/**}'])
+        .pipe(sourcemaps.init())
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(sourcemaps.write('./maps'))
         .pipe(gulp.dest('./public'))
-)
-//css
-gulp.task('minify-css', () => {
-    return gulp.src('./public/**/*.css')
-        .pipe(cleanCSS({
-            compatibility: 'ie11'
-        }))
-        .pipe(gulp.dest('./public'));
-});
+);
 
-
-// 壓縮 public 目錄內 html
-gulp.task('minify-html', () => {
-    return gulp.src('./public/**/*.html')
+// 压缩html文件
+const minify_html = () => (
+    gulp.src(['./public/**/*.html','!./public/{lib,lib/**}','!./public/{libs,libs/**}','!./public/{media,media/**}'])
         .pipe(htmlclean())
         .pipe(htmlmin({
-            removeComments: true, //清除 HTML 註釋
-            collapseWhitespace: true, //壓縮 HTML
-            collapseBooleanAttributes: true, //省略布爾屬性的值 <input checked="true"/> ==> <input />
-            removeEmptyAttributes: true, //刪除所有空格作屬性值 <input id="" /> ==> <input />
-            removeScriptTypeAttributes: true, //刪除 <script> 的 type="text/javascript"
-            removeStyleLinkTypeAttributes: true, //刪除 <style> 和 <link> 的 type="text/css"
-            minifyJS: true, //壓縮頁面 JS
-            minifyCSS: true, //壓縮頁面 CSS
-            minifyURLs: true
+            removeComments: true,
+            minifyJS: true,
+            minifyCSS: true,
+            minifyURLs: true,
         }))
         .pipe(gulp.dest('./public'))
-});
+)
 
-// 執行 gulp 命令時執行的任務
-gulp.task("default", gulp.parallel(
-    'compress','minify-html', 'minify-css'
-));
+// 压缩js文件
+const minify_js = () => (
+    gulp.src(['./public/**/*.js', '!./public/**/*.min.js','!./public/{lib,lib/**}','!./public/{libs,libs/**}','!./public/{media,media/**}'])
+        .pipe(sourcemaps.init())
+        .pipe(terser())
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./public'))
+)
+
+gulp.task('one', gulp.parallel(
+    minify_html,
+    minify_css,
+    minify_js
+))
+
+gulp.task('default', gulp.series('one'));
