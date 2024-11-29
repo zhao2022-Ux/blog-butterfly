2044c34608f38ed265f366e84549091ad1fcfc05
SinzMise <email@sinzmise.top>
2024-11-29 22:07:12 +0800

    修改link.yml文件位置+添加 青桔气球 友情链接

--- deploy-en.sh
+++ deploy-en.sh
@@ -3,4 +3,5 @@
 cp "config-en.yml" _config.yml
 cp "config-butterfly-en.yml" "_config.butterfly.yml"
 cp "gulpfile-en.js" "gulpfile.js"
+cp "links.yml" "source-en/_data/link.yml"
 echo "Set en success!"
--- deploy-zh.sh
+++ deploy-zh.sh
@@ -3,4 +3,5 @@
 cp "config-zh.yml" _config.yml
 cp "config-butterfly-zh.yml" "_config.butterfly.yml"
 cp "gulpfile-zh.js" "gulpfile.js"
+cp "links.yml" "source/_data/link.yml"
 echo "Set zh success!"
