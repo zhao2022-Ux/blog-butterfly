5aa17ff059a1d56dc14c893eb1b27087fbac7183
SinzMise <email@sinzmise.top>
2024-12-01 20:22:15 +0800

    分离会出bug，还是不分离了

--- search-en.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cp "search-en.yml" _config.yml
-cp "config-butterfly-en.yml" "_config.butterfly.yml"
-cp "gulpfile-en.js" "gulpfile.js"
-cp "links.yml" "source-en/_data/link.yml"
-echo "Set en success!"
