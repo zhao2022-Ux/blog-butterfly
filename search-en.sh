b03f3787386e3db911e58fad214e0712907319d2
SinzMise <email@sinzmise.top>
2024-12-01 20:17:10 +0800

    百度搜索push重新开个action

---
+++ search-en.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp "search-en.yml" _config.yml
+cp "config-butterfly-en.yml" "_config.butterfly.yml"
+cp "gulpfile-en.js" "gulpfile.js"
+cp "links.yml" "source-en/_data/link.yml"
+echo "Set en success!"
